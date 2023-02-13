50fc3d07e8fe5259b555d1fa2eb66dd206d3191a
Josh <josh.jennings5@gmail.com>
2023-02-13 20:33:11 +0200

    Added WriteToFIle

--- ReadWrite.cs
+++ ReadWrite.cs
@@ -7,5 +7,20 @@ namespace BirthFinder
             string IDFile = Settings.filePath + Settings.fileName;
             return File.ReadAllLines(IDFile);
         }
+
+        public void WriteToFIle(string line, int birthYear)
+        {
+            string DOBPath = @"C:\BBD\C# Fundamentals\BirthdayFinder\docs\DOB.txt";
+            if(File.Exists(DOBPath))
+            {
+                if(birthYear <= 2010)
+                {
+                    File.AppendAllText(DOBPath, line + " is below 2010 " + Environment.NewLine);
+                }else
+                {
+                    File.AppendAllText(DOBPath, line + " is above 2010" + Environment.NewLine);
+                }
+            }
+        }
     }
 }
