34ff18a018978a36280dad2f4e958e8810e16f03
Josh <josh.jennings5@gmail.com>
2023-02-13 20:23:43 +0200

    Moved Validation to it's own class

--- BirthFinder.cs
+++ BirthFinder.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Text.RegularExpressions;
 namespace BirthFinder
 {
     class IDValidator
@@ -24,52 +23,6 @@ namespace BirthFinder
                 }
 
             }Console.WriteLine("File not found.");
-        }
-
-        public delegate bool Validate(string line);
-
-        private bool ValidationChecks(string line)
-        {    
-            Validate lengthDigitCheck = LengthDigitCheck;
-            Validate luhnCheck = LuhnCheck;
-            return lengthDigitCheck(line) ? (luhnCheck(line) ? true : false) : false;
-        }
-
-        private bool LengthDigitCheck(string line)
-        {
-            Regex regex = new Regex(@"^\d{13}$");
-            return regex.IsMatch(line);
-        }
-
-        private bool LuhnCheck(string line)
-        {
-            long sum = 0;
-
-            for (int i = 0; i < line.Length; i++)
-            {
-                var digit = line[line.Length - 1 - i] - '0';
-                sum += (i % 2 != 0) ? GetDouble(digit) : digit;
-            }
-
-            return sum % 10 == 0;
-
-            int GetDouble(long i)
-            {
-                switch (i)
-                {
-                    case 0: return 0;
-                    case 1: return 2;
-                    case 2: return 4;
-                    case 3: return 6;
-                    case 4: return 8;
-                    case 5: return 1;
-                    case 6: return 3;
-                    case 7: return 5;
-                    case 8: return 7;
-                    case 9: return 9;
-                    default: return 0;
-                }
-            }
         }
 
         public string[] ReadFile()
