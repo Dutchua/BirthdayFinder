2928d4ca4f2cf024766460af922d1f21f420c12f
Josh <josh.jennings5@gmail.com>
2023-02-13 19:07:57 +0200

    Added Luhn Checker.

--- BirthFinder.cs
+++ BirthFinder.cs
@@ -17,7 +17,48 @@ namespace BirthFinder
             if(fileExists)
             {
                 string[] fileContents = ReadFile();
+                foreach(string line in fileContents)
+                {
+                    Console.WriteLine(PassesLuhnCheck(line));
+                }
+
             }Console.WriteLine("File not found.");
+        }
+
+        public void validationChecks()
+        {
+            
+        }
+
+        bool PassesLuhnCheck(string value)
+        {
+            long sum = 0;
+
+            for (int i = 0; i < value.Length; i++)
+            {
+                var digit = value[value.Length - 1 - i] - '0';
+                sum += (i % 2 != 0) ? GetDouble(digit) : digit;
+            }
+
+            return sum % 10 == 0;
+
+            int GetDouble(long i)
+            {
+                switch (i)
+                {
+                    case 0: return 0;
+                    case 1: return 2;
+                    case 2: return 4;
+                    case 3: return 6;
+                    case 4: return 8;
+                    case 5: return 1;
+                    case 6: return 3;
+                    case 7: return 5;
+                    case 8: return 7;
+                    case 9: return 9;
+                    default: return 0;
+                }
+            }
         }
 
         public string[] ReadFile()
