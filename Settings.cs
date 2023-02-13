b2f5108fef187a2666b5757a6bbf5fa37fa1a4cf
Josh <josh.jennings5@gmail.com>
2023-02-13 20:26:45 +0200

    Changed file path for .txt files

--- Settings.cs
+++ Settings.cs
@@ -2,7 +2,7 @@ namespace BirthFinder
 {
     class Settings
     {
-        public const string filePath = @"C:\BBD\C# Fundamentals\BirthdayFinder";
+        public const string filePath = @"C:\BBD\C# Fundamentals\BirthdayFinder\docs";
         public const string fileName = @"\ID.txt";
     }
 }
