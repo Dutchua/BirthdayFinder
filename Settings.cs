d8df34b79433854b220db305be07f38e9f380116
Josh <josh.jennings5@gmail.com>
2023-02-13 17:38:17 +0200

    Added file locations as constants

---
+++ Settings.cs
@@ -0,0 +1,8 @@
+namespace BirthFinder
+{
+    class Settings
+    {
+        public const string filePath = @"C:\BBD\C# Fundamentals\BirthdayFinder";
+        public const string fileName = @"\ID.txt";
+    }
+}
