f4e9f867b2e8dfdd4016e0b6d689d341af60c88f
Josh <josh.jennings5@gmail.com>
2023-02-13 21:39:17 +0200

    Fixed Directory Path

--- Settings.cs
+++ Settings.cs
@@ -2,7 +2,7 @@ namespace BirthFinder
 {
     class Settings
     {
-        public const string FilePath = @"C:\BBD\C# Fundamentals\BirthdayFinder\docs";
+        public const string FilePath = @"C:\BBD\C# Fundamentals\BirthdayFinder\doc";
         public const string IDFileName = @"\ID.txt";
         public const string DOBFileName = @"\DOB.txt";
     }
